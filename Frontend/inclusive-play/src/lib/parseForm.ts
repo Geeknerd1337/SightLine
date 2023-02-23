fe67180064bad2652388ab7f98bd6c120ba9af44
Geeknerd1337 <geeknerd1337@gmail.com>
2023-02-23 14:45:04 -0600

    Fix vercel deployment errors

--- Frontend/inclusive-play/src/lib/parseForm.ts
+++ Frontend/inclusive-play/src/lib/parseForm.ts
@@ -5,7 +5,7 @@ import { IncomingMessage } from "http";
 const parseForm = async (
   form: IncomingForm,
   req: IncomingMessage
-): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
+): Promise<{ fields: any; files: any }> => {
   return await new Promise(async (resolve, reject) => {
     form.parse(req, function (err, fields, files) {
       if (err) return reject(err);
