7f6fbc356800c1b66f0da2a9db96b90a8c992225
Sam Fisher <sammee1@gmail.com>
2023-02-27 17:06:19 -0600

    Added links
    -button style adjustments

--- Frontend/inclusive-play/src/components/DropDownButton.tsx
+++ Frontend/inclusive-play/src/components/DropDownButton.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { useState } from "react"
-import { AiOutlineCaretUp, AiOutlineCaretDown } from "react-icons/ai"
+import { AiOutlineCaretUp, AiOutlineCaretDown, AiOutlineFileAdd } from "react-icons/ai"
 import list from "@/list.json"
 
 export default function DropDownButton() {
     const [isOpen, setIsOpen] = useState(false)
 
-    return <div className="relative flex flex-col items-center h-[300px] rounded-md">
+    return <div className="relative flex flex-col items-center h-[300px] rounded-md mx-3">
         <button onClick={() => setIsOpen((prev) => !prev)} className="bg-purple-300 p-4 w-full flex items-center justify-between font-bold text-lg rounded-md">
+            <AiOutlineFileAdd className="h-7 w-7 ml-16 mb-1"/>
             Submit File
             {!isOpen ? (
-                <AiOutlineCaretDown className="h-8" />
+                <AiOutlineCaretDown className="h-8 ml-14" />
             ): (
-                <AiOutlineCaretUp className="h-8" />
+                <AiOutlineCaretUp className="h-8 ml-14" />
             )}
         </button>
 
--- Frontend/inclusive-play/src/views/HomeView.tsx
+++ Frontend/inclusive-play/src/views/HomeView.tsx
@@ -2,6 +2,8 @@
 
 import RoundedCard from "@/components/RoundedCard"
 import DropDownButton from "@/components/DropDownButton"
+import links from "@/links.json"
+import { AiOutlineLink } from "react-icons/ai"
 
 // * home page
 export default function HomeView() {
@@ -22,13 +24,21 @@ export default function HomeView() {
 
 				<DropDownButton></DropDownButton>
 				
-
 				<RoundedCard className="m-3" />
-				<RoundedCard className="m-3" >
+				<RoundedCard className="m-3">
 					<div className="flex pb-4 font-semibold text-2xl justify-items-start flex-row">
 						<RoundedCard className="bg-red-300 mr-3 p-2.5 md:filter-none"/>	
 						Links
 					</div>
+
+					<ul className="flex flex-col items-start">
+						{links.map((link, i) => (
+							<div className="flex items-center justify-between p-1" key={i}>
+								<AiOutlineLink className="mr-1"/>
+								<li className="font-semibold text-lg hover:text-purple-400"><a href={link.URL} target="_blank">{link.title}</a></li>	
+							</div>
+						))}
+					</ul>
 				</RoundedCard>
 			</div>
 		)
@@ -56,6 +66,11 @@ export default function HomeView() {
 						<RoundedCard className="bg-sky-300 mr-3 p-2.5 md:filter-none"/>	
 						Grade Breakdown
 					</div>
+
+					<RoundedCard className="border flex flex-auto drop-shadow-mg h-1/2 p-4 p-top">
+						<p></p>
+					</RoundedCard>
+
 				</RoundedCard>
 			</>
 		)
@@ -65,8 +80,8 @@ export default function HomeView() {
 	return (
 		<>
 			<div className="h-20">{renderSubHeader()}</div>
-			<div className="flex-1 flex m-8 gap-8 flex-row text-black">
-				<div className="w-72 flex flex-col">{renderLeftColumn()}</div>
+			<div className="flex-1 flex m-8 gap-6 flex-row text-black">
+				<div className="w-74 flex flex-col">{renderLeftColumn()}</div>
 				<div className="flex-auto flex flex-col grow-2">{renderMiddleColumn()}</div>
 				<div className="flex-auto flex flex-col">{renderRightColumn()}</div>
 			</div>
