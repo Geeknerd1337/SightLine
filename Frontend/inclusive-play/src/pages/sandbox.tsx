ddc473ae8b0af9037409960f47a02d7d5e85d28d
Geeknerd1337 <geeknerd1337@gmail.com>
2023-03-03 00:44:16 -0600

    AWS Lamda Function Test

--- Frontend/inclusive-play/src/pages/sandbox.tsx
+++ Frontend/inclusive-play/src/pages/sandbox.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+//Import keys.json from lib
+import keys from "../lib/secrets/keys.json";
 
 export default function App() {
   // const [file, setFile] = useState<any>(null);
@@ -29,20 +31,42 @@ export default function App() {
   // );
 
   const [number, setNumber] = useState(0);
-  const [incremented, setIncremented] = useState(null);
+  const [result, setResult] = useState(null);
 
   const handleIncrement = async () => {
-    const response = await fetch(`/api/counter?number=${number}`);
-    const data = await response.json();
-    setIncremented(data.incremented);
-    setNumber(data.incremented);
+    try {
+      console.log(JSON.stringify({ number }));
+      const response = await fetch(
+        "https://kzv0hg8e9a.execute-api.us-east-1.amazonaws.com/default/counterFunction",
+        {
+          method: "POST",
+          headers: {
+            "x-api-key": keys["aws-counter-function-key"],
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ number }),
+          mode: "cors",
+        }
+      );
+
+      const data = await response.json();
+      console.log(data);
+      setNumber(data.result);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div>
-      <p>Number: {number}</p>
-      {incremented && <p>Incremented: {incremented}</p>}
+      <h1>Increment Number</h1>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(Number(e.target.value))}
+      />
       <button onClick={handleIncrement}>Increment</button>
+      <p>Result: {result}</p>
     </div>
   );
 }
