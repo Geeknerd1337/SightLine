61224611d97a5d6fc89659cd1d65d25096fa97f2
Geeknerd1337 <geeknerd1337@gmail.com>
2023-08-30 13:03:35 -0500

    Add Colors to Styles

---
+++ src/styles/colors.ts
@@ -0,0 +1,18 @@
+const Back = '#1E1E1E';
+const Gold = "#FFD166";
+const White = "#FFFFFF";
+const Gray = "#303030";
+const DarkGray = "#252525";
+const Text = "#F2F2F2";
+const DarkText = "#DDDDDD";
+
+
+export const Colors = {
+  Back,
+  Gold,
+  White,
+  Gray,
+  DarkGray,
+  Text,
+  DarkText
+};
