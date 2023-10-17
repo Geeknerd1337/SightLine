9a1c85c1aa3ce87dbc0b06f53b7ab95adfdc4cb6
Sam Fisher <sammee1@gmail.com>
2023-10-17 16:29:25 -0500

    removed test components

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -1,14 +1,9 @@
-import WaveForm from '@/components/Video Analysis/WaveFormTest';
-import WaveFormVideo from '@/components/Video Analysis/WaveFormVideo';
-import { Background } from '@/styles/HomeViewStyles';
+import { Background } from "@/styles/HomeViewStyles";
 
 export default function Sandbox() {
   return (
     <div>
-      <Background>
-        <WaveFormVideo></WaveFormVideo>
-        <WaveForm></WaveForm>
-      </Background>
+      <Background></Background>
     </div>
   );
 }
